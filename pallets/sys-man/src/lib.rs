0d4cf91658573289f9584a26fdc5ff00baa0c116
zunvu <vudung.siaedu@gmail.com>
2022-02-03 22:56:21 +0700

    implement sys-man features

--- pallets/sys-man/src/lib.rs
+++ pallets/sys-man/src/lib.rs
@@ -13,12 +13,33 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use frame_support::{dispatch::DispatchResultWithPostInfo, ensure, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+	use pallet_utils::{Role, Status};
+	use scale_info::TypeInfo;
+	// use serde::{Deserialize, Serialize};
+	use serde_json::{json, Value};
+
+	pub enum OperationType {
+		SYS,
+		ORG,
+	}
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[derive(Decode, Encode, Clone, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+	#[scale_info(bounds(), skip_type_params(T))]
+	pub struct SysManAccount<T: Config> {
+		// id: T::AccountId,
+		pub role: Role,
+		pub status: Status,
+		pub level: Option<u8>,
+		pub parent: Option<T::AccountId>,
+		pub children: Option<Vec<T::AccountId>>,
+		pub metadata: String,
 	}
 
 	#[pallet::pallet]
@@ -26,38 +47,289 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::storage]
-	pub type SysMan<T> = StorageMap<_, BlakeTwo256, _, _>;
+	pub type SysMan<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, SysManAccount<T>, OptionQuery>;
 
 	#[pallet::storage]
-	pub type SysMapRevoked<T> = StorageMap<_, BlakeTwo256, _, _>;
+	pub type SysManRevoked<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, SysManAccount<T>, OptionQuery>;
 
 	#[pallet::storage]
-	pub type Org<T> = StorageMap<_, BlakeTwo256, _, _>;
+	pub type Org<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, SysManAccount<T>, OptionQuery>;
 
 	#[pallet::storage]
-	pub type OrgRevoked<T> = StorageMap<_, BlakeTwo256, _, _>;
+	pub type OrgRevoked<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, SysManAccount<T>, OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn sys_man_cnt)]
+	/// Keeps track of the number of system managers in existence.
+	pub(super) type SysManCnt<T: Config> = StorageValue<_, u64, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn org_cnt)]
+	/// Keeps track of the number of system managers in existence.
+	pub(super) type OrgCnt<T: Config> = StorageValue<_, u64, ValueQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		Created,
+		Approved { target_id: T::AccountId, metadata: Vec<u8>, approver: T::AccountId },
+		Revoked { target_id: T::AccountId, revoker: T::AccountId },
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		NoneValue,
+		NoValidAuthorization,
 		CreateFailed,
+		AlreadyRegistered,
+		AlreadyRevoked,
+		ConvertMetadataFailed,
+		RetrieveAuthorityFailed,
+		SysManNotExist,
+		RevokedSysManNotExist,
+		RevokedOrgNotExist,
+		OperationTypeInvalid,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(10_000)]
-		pub fn add_sys_man(
+		pub fn approve_sys_man(
 			origin: OriginFor<T>,
-			id: T::AccountId,
-			des: Vec<u8>,
+			sys_man_id: T::AccountId,
+			metadata: Vec<u8>,
 		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			// ensure extrinsics caller has right permission
+			let authority = Self::get_account(&sender, OperationType::SYS)?;
+
+			// check whether sys man has been approved
+			ensure!(!SysMan::<T>::contains_key(&sys_man_id), Error::<T>::AlreadyRegistered);
+
+			// check whether sys man has been revoked
+			ensure!(!SysManRevoked::<T>::contains_key(&sys_man_id), Error::<T>::AlreadyRevoked);
+
+			// create system manager account
+			let sys_man = Self::create_account(
+				Role::SysMan,
+				Status::Active,
+				Some(authority.level.unwrap() + 1),
+				Some(vec![]),
+				Some(sender.clone()),
+				metadata.clone(),
+			)
+			.unwrap();
+
+			SysMan::<T>::insert(&sys_man_id, sys_man);
+
+			Self::deposit_event(Event::<T>::Approved {
+				target_id: sys_man_id,
+				metadata,
+				approver: sender,
+			});
+
 			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn approve_org(
+			origin: OriginFor<T>,
+			org_id: T::AccountId,
+			metadata: Vec<u8>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			// check permisisno of authority
+			let _ = Self::get_account(&sender, OperationType::SYS)?;
+
+			// check whether org has been approved
+			ensure!(!Org::<T>::contains_key(&org_id), Error::<T>::AlreadyRegistered);
+
+			// check whether org has been revoked
+			ensure!(!OrgRevoked::<T>::contains_key(&org_id), Error::<T>::AlreadyRevoked);
+
+			// create system manager account
+			let org_account = Self::create_account(
+				Role::Organization,
+				Status::Active,
+				None,
+				None,
+				None,
+				metadata.clone(),
+			)
+			.unwrap();
+
+			Org::<T>::insert(&org_id, org_account);
+
+			Self::deposit_event(Event::<T>::Approved {
+				target_id: org_id,
+				metadata,
+				approver: sender,
+			});
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn revoke_org(
+			origin: OriginFor<T>,
+			revoke_org_id: T::AccountId,
+			description: Vec<u8>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			// check permission of revoker
+			let _ = Self::get_account(&sender, OperationType::SYS)?;
+
+			let mut revoke_org = Self::get_account(&revoke_org_id, OperationType::ORG)?;
+
+			// ensure revoked org has not been revoked yet
+			ensure!(!OrgRevoked::<T>::contains_key(&revoke_org_id), Error::<T>::AlreadyRevoked);
+
+			// add revoked description to metadata string
+			let mut metadata = json!(revoke_org.metadata);
+
+			metadata = Self::add_json_field(
+				&metadata,
+				"revoke_description".to_string(),
+				&String::from_utf8(description).unwrap(),
+			);
+
+			revoke_org.metadata = metadata.to_string();
+
+			// remove revoked sys man from Org Storage
+			Org::<T>::remove(&revoke_org_id);
+
+			// add revoked sys man to OrgRevoked Storage
+			OrgRevoked::<T>::insert(&revoke_org_id, revoke_org);
+
+			// emit revoked event with information of revoked sys man
+			Self::deposit_event(Event::<T>::Revoked { target_id: revoke_org_id, revoker: sender });
+
+			Ok(().into())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn revoke_sys_man(
+			origin: OriginFor<T>,
+			revoke_id: T::AccountId,
+			description: Vec<u8>,
+		) -> DispatchResultWithPostInfo {
+			let sender = ensure_signed(origin)?;
+
+			let authority = Self::get_account(&sender, OperationType::SYS)?;
+
+			let mut revoke_sys_man = Self::get_account(&revoke_id, OperationType::SYS)?;
+
+			// Ensure authority has higher hierarchical level than system manager to be revoked
+			ensure!(
+				authority.level.unwrap_or(0) < revoke_sys_man.level.unwrap_or(0),
+				Error::<T>::NoValidAuthorization
+			);
+
+			// ensure revoked sys man has not been revoked yet
+			ensure!(!SysManRevoked::<T>::contains_key(&revoke_id), Error::<T>::AlreadyRevoked);
+
+			// add revoked description to metadata string
+
+			let mut metadata = json!(revoke_sys_man.metadata);
+
+			metadata = Self::add_json_field(
+				&metadata,
+				"revoke_description".to_string(),
+				&String::from_utf8(description).unwrap(),
+			);
+
+			// metadata["revoke_description"] = Value::String(String::from_utf8(description).unwrap());
+
+			revoke_sys_man.metadata = metadata.to_string();
+
+			// remove revoked sys man from SysMan Storage
+			SysMan::<T>::remove(&revoke_id);
+
+			// add revoked sys man to SysManRevoked Storage
+			SysManRevoked::<T>::insert(&revoke_id, revoke_sys_man);
+
+			// emit revoked event with information of revoked sys man
+			Self::deposit_event(Event::<T>::Revoked { target_id: revoke_id, revoker: sender });
+
+			Ok(().into())
+		}
+	}
+
+	// private functions
+	impl<T: Config> Pallet<T> {
+		pub fn create_account(
+			role: Role,
+			status: Status,
+			level: Option<u8>,
+			children: Option<Vec<T::AccountId>>,
+			parent: Option<T::AccountId>,
+			metadata: Vec<u8>,
+		) -> Result<SysManAccount<T>, Error<T>> {
+			let metadata_string = match String::from_utf8(metadata) {
+				Ok(v) => v,
+				Err(_) => Err(Error::<T>::ConvertMetadataFailed)?,
+			};
+
+			// TODO: validate metadata to be a valid JSON string
+
+			let sys_man = SysManAccount::<T> {
+				role,
+				status,
+				level,
+				children,
+				parent,
+				metadata: metadata_string,
+			};
+
+			Ok(sys_man)
+		}
+
+		pub fn get_account(
+			id: &T::AccountId,
+			op_type: OperationType,
+		) -> Result<SysManAccount<T>, Error<T>> {
+			let authority = match op_type {
+				OperationType::SYS => match SysMan::<T>::try_get(id) {
+					Ok(val) => match val.role {
+						Role::SysMan => val,
+						_ => Err(Error::<T>::NoValidAuthorization)?,
+					},
+					Err(_) => Err(Error::<T>::SysManNotExist)?,
+				},
+
+				OperationType::ORG => match Org::<T>::try_get(id) {
+					Ok(val) => match val.role {
+						Role::Organization => val,
+						_ => Err(Error::<T>::RevokedOrgNotExist)?,
+					},
+					Err(_) => Err(Error::<T>::RevokedOrgNotExist)?,
+				},
+			};
+
+			Ok(authority)
+		}
+
+		pub fn str2vec(s: &str) -> Vec<u8> {
+			s.as_bytes().to_vec()
+		}
+
+		fn add_json_field(v: &Value, field_key: String, field_val: &str) -> Value {
+			match v {
+				Value::Object(map) => {
+					let mut map = map.clone();
+
+					map.insert(field_key.clone(), Value::String(field_val.to_string()));
+
+					Value::Object(map)
+				},
+				v => v.clone(),
+			}
 		}
 	}
 }
